cd608d5214910529e07daf70eed57f460080a0a2
Kanaka Raju <43216503+r-aju@users.noreply.github.com>
2021-07-16 17:06:36 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-
 #!/bin/bash
+
+echo "---------------------------------------------------"
+echo "Hello Please find the Hardware Specifications Below"
+echo "---------------------------------------------------"
 
 os_version()
 {
@@ -14,25 +17,23 @@ kernel_version()
 {
 echo ''
 echo "The Kernel Version is :"
-uname -r
-echo ''
+kernel_version=`uname -r`
+echo $kernel_version
 }
-
-
 
 cpu_clock_speed()
 {
 echo ''
 echo "The CPU Clock Speed is :"
-cat /proc/cpuinfo >> c.txt | cat c.txt | sed -n 8p
+cat /proc/cpuinfo | sed -n 8p
 echo ''
-}
 
 cpu_architecture()
 {
 echo ''
 echo "The CPU Architectute :"
-lscpu | grep Architecture
+cpu_architecture=`lscpu | grep Architecture`
+echo $cpu_architecture
 echo ''
 }
 
@@ -47,28 +48,81 @@ echo ''
 disk_details()
 {
 echo ''
-echo "Disk_details"
-lsblk
+echo "Mounted partition"
+echo ''
+blkid -c /dev/nul | awk -F: '{print $1}'
+echo ''
+echo "Unmounted Partition"
+lsblk  --noheadings --raw | awk '{print substr($0,0,4)}' | uniq -c | grep 1 | awk '{print "/dev/"$2}'
 echo ''
 }
-
 
 physical_cpu_cores()
 {
 echo ''
-echo "Physical CPU Cores"
-cat /proc/cpuinfo | grep "cpu cores"
+echo "Physical and Virtual CPU Cores"
+phy_cores=`grep "physical id" /proc/cpuinfo | sort | uniq | wc -l`
+vir_cores=`grep "^processor" /proc/cpuinfo | wc -l`
+echo "The Ratio is"
+echo $vir_cores ":" $phy_cores
 echo ''
 }
 
+network_ports()
+{
+echo ''
+echo "The list of active protocols and ports are"
+netstat -tulpn | awk '{print $1,$4}'
+echo ''
+}
+
+os_firewalls()
+{
+echo ''
+cat /etc/services| grep -E 'tcp|udp'
+echo ''
+}
+
+network_bandwidth()
+{
+echo ''
+echo "Networking Bandwidth"
+bwm-ng -o csv -c 6 -T rate -I etho,enp0s3
+echo ''
+}
+
+echo "---------------------------------------------------"
 
 
+echo "---------------------------------------------------"
 os_version
+
+echo "---------------------------------------------------"
 kernel_version
+
+echo "---------------------------------------------------"
 cpu_clock_speed
+
+echo "---------------------------------------------------"
 cpu_architecture
+
+echo "---------------------------------------------------"
 ram_details
+
+echo "---------------------------------------------------"
 disk_details
+
+echo "---------------------------------------------------"
 physical_cpu_cores
 
+echo "---------------------------------------------------"
+network_ports
+
+echo "---------------------------------------------------"
+os_firewalls
+
+echo "---------------------------------------------------"
+network_bandwidth
+
+echo "---------------------------------------------------"
 
